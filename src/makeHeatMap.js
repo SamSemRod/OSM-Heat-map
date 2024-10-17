19cfed05dc497af3b46a0f80ef4c2ede2b3bb89b
SamSemRod <semyon.rodionov.03@mail.ru>
2024-10-17 11:51:12 +0300

    flats fetch query modified

--- src/makeHeatMap.js
+++ src/makeHeatMap.js
@@ -1,7 +1,8 @@
 /* eslint-disable camelcase */
-const all_points = [];
+let all_points = [];
 const flats_points = [];
 const levels_points = [];
+const shop_points = [];
 
 function countFlats(inputStr) {
   let res_incude = 0;
@@ -17,86 +18,99 @@ function countFlats(inputStr) {
   return res_incude;
 }
 
-async function fetchFlatsFromOverpassAPI() {
-  const overpassFlatsURL = `https://maps.mail.ru/osm/tools/overpass/api/interpreter?data=[out:json][timeout:180];
+async function fetchShops() {
+  const overpassShopsURL = `https://maps.mail.ru/osm/tools/overpass/api/interpreter?data=[out:json];
   (
-    way["building:flats"](59.7590, 30.0882, 60.1085, 30.7603);
-  ) -> .buildings;
-  way.buildings; out center tags;`;
+    node[name~"пят(е|ё)рочка",i](59.7590, 30.0882, 60.1085, 30.7603);
+    node[name="Магнит"](59.7590, 30.0882, 60.1085, 30.7603);
+    node[name="Семишагофф"](59.7590, 30.0882, 60.1085, 30.7603);
+    node[name="Дикси"](59.7590, 30.0882, 60.1085, 30.7603););out;`;
 
   try {
-    const response = await fetch(overpassFlatsURL);
+    const response = await fetch(overpassShopsURL);
     const data = await response.json();
-    console.log('Flats data:', data);
+    console.log('Shops data:', data);
 
     data.elements.forEach((element) => {
-      if (element.center && element.tags['building:flats']) {
-        flats_points.push([
-          element.center.lat,
-          element.center.lon,
-          parseInt(element.tags['building:flats'], 10) / 100,
-        ]);
+      if (
+        element.tags.shop
+        && (element.tags.shop === 'supermarket'
+          || element.tags.shop === 'convenience')
+      ) {
+        shop_points.push([
+          element.lat,
+          element.lon,
+          element.tags.brand]);
       }
     });
 
-    console.log('Flats points fetched:', flats_points);
+    console.log('Shops points fetched:', shop_points);
   } catch (error) {
-    console.error('Error fetching flats data from Overpass API:', error);
+    console.error('Error fetching shops data from Overpass API:', error);
   }
 }
 
-async function fetchLevelsFromOverpassAPI() {
-  const overpassLevelsURL = `https://maps.mail.ru/osm/tools/overpass/api/interpreter?data=[out:json][timeout:180];
-  (
-    way["building:levels"](59.7590, 30.0882, 60.1085, 30.7603);
-  ) -> .buildings;
-  way.buildings; out center tags;`;
-
-  try {
-    const response = await fetch(overpassLevelsURL);
-    const data = await response.json();
-    console.log('Levels data:', data);
-
-    data.elements.forEach((element) => {
-      if (element.center && element.tags['building:levels']) {
-        const levels = parseInt(element.tags['building:levels'], 10);
-        const flats = levels * 30;
-        levels_points.push([element.center.lat, element.center.lon, flats / 100]);
-      }
-    });
-
-    console.log('Levels points fetched:', levels_points);
-  } catch (error) {
-    console.error('Error fetching levels data from Overpass API:', error);
-  }
-}
-
-async function fetchPointsFromOverpassAPI() {
+async function fetchDataAndFilter() {
   const overpassURL = `https://maps.mail.ru/osm/tools/overpass/api/interpreter?data=[out:json];
-  (node["entrance"="staircase"](59.7590, 30.0882, 60.1085, 30.7603);
-  node["addr:flats"](59.7590, 30.0882, 60.1085, 30.7603);
-  node["entrance"="yes"]["ref"](59.7590, 30.0882, 60.1085, 30.7603);
-  node[name~"пят(е|ё)рочка",i](59.7590, 30.0882, 60.1085, 30.7603);
-  node[name="Магнит"](59.7590, 30.0882, 60.1085, 30.7603);
-  node[name="Семишагофф"](59.7590, 30.0882, 60.1085, 30.7603);
-  node[name="Дикси"](59.7590, 30.0882, 60.1085, 30.7603););out;`;
+  (way["building:levels"][building=apartments](59.7590,30.0882,60.1085,30.7603);
+  node["entrance"="staircase"](59.7590,30.0882,60.1085,30.7603);
+  node["addr:flats"](59.7590,30.0882,60.1085,30.7603);
+  node["entrance"="yes"]["ref"](59.7590,30.0882,60.1085,30.7603);
+  way["building:flats"](59.7590,30.0882,60.1085,30.7603););out center tags;`;
 
   try {
     const response = await fetch(overpassURL);
     const data = await response.json();
-    console.log(data);
+
+    const buildingsWithFlats = {};
+    const buildingsWithEntrances = {};
+    const buildingsWithLevels = {};
 
     data.elements.forEach((element) => {
-      if (element.lat && element.lon && element.tags['addr:flats']) {
-        all_points.push([
-          element.lat,
-          element.lon,
-          countFlats(element.tags['addr:flats']),
-        ]);
+      if (element.type === 'way' && element.tags['building:flats']) {
+        const buildingKey = `${element.tags['addr:housenumber']}_${element.tags['addr:street']}`;
+        buildingsWithFlats[buildingKey] = {
+          lat: element.center.lat,
+          lon: element.center.lon,
+          flats: parseInt(element.tags['building:flats'], 10),
+        };
       }
     });
 
-    console.log('Points fetched from Overpass API:', all_points);
+    data.elements.forEach((element) => {
+      if (element.type === 'node' && element.tags.entrance) {
+        const buildingKey = `${element.tags['addr:housenumber']}_${element.tags['addr:street']}`;
+        if (!buildingsWithFlats[buildingKey]) {
+          buildingsWithEntrances[buildingKey] = {
+            lat: element.lat,
+            lon: element.lon,
+          };
+        }
+      }
+    });
+
+    data.elements.forEach((element) => {
+      if (element.type === 'way' && element.tags['building:levels']) {
+        const buildingKey = `${element.tags['addr:housenumber']}_${element.tags['addr:street']}`;
+        if (!buildingsWithFlats[buildingKey] && !buildingsWithEntrances[buildingKey]) {
+          const levels = parseInt(element.tags['building:levels'], 10);
+          const flats = levels * 30;
+          buildingsWithLevels[buildingKey] = {
+            lat: element.center.lat,
+            lon: element.center.lon,
+            flats,
+          };
+        }
+      }
+    });
+
+    all_points = [
+      ...Object.values(buildingsWithFlats),
+      ...Object.values(buildingsWithEntrances),
+      ...Object.values(buildingsWithLevels),
+    ];
+
+    console.log(all_points);
   } catch (error) {
     console.error('Error fetching data from Overpass API:', error);
   }
@@ -111,19 +125,29 @@ async function initializeMap() {
   }).addTo(map);
 
   await Promise.all([
-    fetchPointsFromOverpassAPI(),
-    fetchFlatsFromOverpassAPI(),
-    fetchLevelsFromOverpassAPI(),
+    fetchDataAndFilter(),
+    fetchShops(),
   ]);
 
   const final_points = all_points.concat(flats_points, levels_points);
   console.log('Combined points:', final_points);
 
-  L.heatLayer(final_points, {
+  L.heatLayer(all_points, {
     radius: 25,
     minOpacity: 0.3,
     gradient: { 0.4: 'blue', 0.65: 'lime', 1: 'red' },
   }).addTo(map);
+
+  const smallIcon = L.icon({
+    iconUrl: 'icons8-маркер-24.png',
+    iconSize: [20, 20],
+    iconAnchor: [10, 20],
+  });
+  shop_points.slice(0, 1000).forEach((point) => {
+    L.marker([point[0], point[1]], { icon: smallIcon })
+      .addTo(map)
+      .bindPopup(point[2]);
+  });
 }
 
 initializeMap();
